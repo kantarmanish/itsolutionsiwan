16afe5949c6e6a375500570f2bdf2cea595a401f
codemanishh <talk2manish.me@gmail.com>
2025-01-18 11:53:05 +0530

    google map added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,8 @@ import Back from "../common/back/Back"
 import "./contact.css"
 
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  const map ='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57270.71088177351!2d84.2805356486328!3d26.215557899999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3992fc006151d267%3A0x5df69c14d63ef24c!2sIt%20solutions%20computer%20center!5e0!3m2!1sen!2sin!4v1737181237653!5m2!1sen!2sin';
+
   return (
     <>
       <Back title='Contact us' />
